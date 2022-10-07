521f9c279cea54f9d2b133d209f485c3f930f54d
unknown <saradawsone@gmail.com>
2022-10-07 13:05:52 -0600

    troubleshooting

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,29 @@
+import LoginForm from'./LoginForm';
+import React from 'react';
+import Spinner from './Spinner';
+import { render, screen, fireEvent } from '@testing-library/react'; 
+
+
+const usernameInput = () => screen.queryByPlaceholderText('Enter username');
+const passwordInput = () => screen.queryByPlaceholderText('Enter password');
+const loginBtn = () => screen.queryByText('Submit credentials');
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true);
+})
+
+test('renders spinner without errors', () => {
+  render(<Spinner />);
+})
+
+test('spinner renders when login button is clicked', () => {
+  render(<LoginForm />);
+  render(<Spinner />);
+  fireEvent.change(usernameInput(), {target: {value: 'bloom'}});
+  fireEvent.change(passwordInput(), { target: { value: '12345678' }});
+  fireEvent.click(loginBtn());
+  const spinner = screen.queryByTestId('spinner');
+  expect(spinner).toBeInTheDocument();
+
 })
